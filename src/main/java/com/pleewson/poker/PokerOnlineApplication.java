bbb35538e98faeffbcea0416e40d369b6c774574
pleewson <adrianplewait@gmail.com>
2024-06-08 10:54:14 +0200

    init

---
+++ src/main/java/com/pleewson/poker/PokerOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.pleewson.poker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pleewson/poker/PokerOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pleewson.poker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
